8ca96d3842bdb075c41c15a17cc9fc36424c3bcf
wesleyaceleraprogramador <wesley@aceleraprogramador.com.br>
2024-11-26 14:54:22 -0300

    alterando estrutura de pastas de projeto para uma pasta unica

--- fundamentos-programacao/desenvolvimento-backend/gerenciamento-pedidos-spring/aula-12/src/main/java/br/com/aceleraprogramador/gerenciamento_pedidos/exceptions/RecursoNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package br.com.aceleraprogramador.gerenciamento_pedidos.exceptions;
-
-public class RecursoNaoEncontradoException extends RuntimeException {
-
-    public RecursoNaoEncontradoException(String message) {
-        super(message);
-    }
-}
